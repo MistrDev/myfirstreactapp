2e0464ef10110e9a61744b7cbd53bae1468f85f1
MistrDev <cwcorster123@gmail.com>
2021-03-04 07:26:41 -0600

    getting my first react app set up and made a couple simple componets and also just learning

---
+++ src/tweet.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function Tweet(){
+    return(
+        <div className='tweet'>
+            <h3>Cory J.</h3>
+            <p>Random Tweet</p>
+            <h3>Number of likes</h3>
+        </div>
+    );
+}
+
+export default Tweet;
