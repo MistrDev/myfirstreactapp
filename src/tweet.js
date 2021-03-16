0fc4631f0b2061f783940195b52c632833feae3e
MistrDev <cwcorster123@gmail.com>
2021-03-16 15:38:15 -0500

    just added a colon to visualize a message

--- src/tweet.js
+++ src/tweet.js
@@ -5,7 +5,7 @@ function Tweet(){
     return(
         <div className='tweet'>
             <h3>Cory J.</h3>
-            <p>Random Message</p>
+            <p>Random Message:</p>
             <h3>I love react so far!</h3>
         </div>
     );
