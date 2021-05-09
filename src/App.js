514ffff2b250329b1e59ddd045e0e0d6d4f9a928
MistrDev <cwcorster123@gmail.com>
2021-05-08 19:03:55 -0500

    got rid of the example componet I made awhiel ago

--- src/App.js
+++ src/App.js
@@ -2,13 +2,6 @@ import React from 'react';
 import Tweet from './tweet';
 
 
-function App(){
-return(
-    <div className='app'>
-      <h1>Hello React!</h1>
-      <Tweet/>
-    </div>
-  );
-}
+
 
 export default App;
